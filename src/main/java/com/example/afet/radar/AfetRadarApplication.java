c7d0c98c8633d50280f743b4bfe56842301bd458
ORHAN TÜRKMENOĞLU <orhantrkmn749@gmail.com>
2025-04-14 19:25:42 +0300

    initial commit

---
+++ src/main/java/com/example/afet/radar/AfetRadarApplication.java
@@ -0,0 +1,13 @@
+package com.example.afet.radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfetRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfetRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/afet/radar/AfetRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.afet.radar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfetRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
